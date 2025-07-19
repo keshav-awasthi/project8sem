8b78647e4187a71a3a3f100d719c681be3498479
keshav-awasthi <keshav.raj.awasthi07@gmail.com>
2025-07-19 16:33:33 +0545

    face verification done

--- project8sem/staticfiles/myapp/js/alert.js
+++
@@ -1,9 +0,0 @@
-
-// Hide the alert box after 3 seconds
-setTimeout(function() {
-    var alert = document.getElementById('message-container');
-    if (alert) {
-        alert.style.display = 'none';
-    }
-}, 5000);
-
